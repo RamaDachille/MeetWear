f2c0a630e8a35f9508a05f751633d4ecc26a0254
RamaDachille dachille108@gmail.com <dachille108@gmail.com>
2023-06-12 14:31:52 +0200

    deleted migration file

--- db/schema.rb
+++ db/schema.rb
@@ -152,6 +152,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_102119) do
     t.string "first_name"
     t.string "last_name"
     t.string "user_name"
+    t.string "profile_picture"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

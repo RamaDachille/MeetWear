d636c4eac885f87a5e8a71248e4368c07bdff4db
RamaDachille dachille108@gmail.com <dachille108@gmail.com>
2023-06-14 17:50:13 +0200

    dependant destroy for comment likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   acts_as_favoritor
-  
+
   has_many :notifications, dependent: :destroy
   has_many :followers, through: :user_followers
   has_many :followed, through: :user_followers
@@ -14,9 +14,10 @@ class User < ApplicationRecord
   has_many :followers, dependent: :destroy
   has_many :followed, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :saved_posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
   has_many :feedbacks, dependent: :destroy
   has_many :posts, dependent: :destroy
 
+  has_many :comments, dependent: :destroy
+  has_many :saved_posts, dependent: :destroy
+  has_many :comment_likes, dependent: :destroy
 end
